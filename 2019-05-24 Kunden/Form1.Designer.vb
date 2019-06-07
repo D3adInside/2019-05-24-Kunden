797428bc7bbe17874ea33bdf46820b0a30683d02
Benedikt Michael Schneider <bs762448@fh-muenster.de>
2019-06-07 09:13:40 +0200

    .

--- 2019-05-24 Kunden/Form1.Designer.vb
+++ 2019-05-24 Kunden/Form1.Designer.vb
@@ -22,10 +22,66 @@ Partial Class Form1
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.cmdDaten = New System.Windows.Forms.Button()
+        Me.cmdAddGuthaben = New System.Windows.Forms.Button()
+        Me.lbKdDaten = New System.Windows.Forms.Label()
+        Me.lbKdGuthaben = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'cmdDaten
+        '
+        Me.cmdDaten.Location = New System.Drawing.Point(229, 22)
+        Me.cmdDaten.Name = "cmdDaten"
+        Me.cmdDaten.Size = New System.Drawing.Size(390, 81)
+        Me.cmdDaten.TabIndex = 0
+        Me.cmdDaten.Text = "Daten füllen"
+        Me.cmdDaten.UseVisualStyleBackColor = True
+        '
+        'cmdAddGuthaben
+        '
+        Me.cmdAddGuthaben.Location = New System.Drawing.Point(229, 261)
+        Me.cmdAddGuthaben.Name = "cmdAddGuthaben"
+        Me.cmdAddGuthaben.Size = New System.Drawing.Size(379, 139)
+        Me.cmdAddGuthaben.TabIndex = 1
+        Me.cmdAddGuthaben.Text = "Guthaben addieren"
+        Me.cmdAddGuthaben.UseVisualStyleBackColor = True
+        '
+        'lbKdDaten
+        '
+        Me.lbKdDaten.AutoSize = True
+        Me.lbKdDaten.Location = New System.Drawing.Point(399, 141)
+        Me.lbKdDaten.Name = "lbKdDaten"
+        Me.lbKdDaten.Size = New System.Drawing.Size(57, 20)
+        Me.lbKdDaten.TabIndex = 2
+        Me.lbKdDaten.Text = "Label1"
+        '
+        'lbKdGuthaben
+        '
+        Me.lbKdGuthaben.AutoSize = True
+        Me.lbKdGuthaben.Location = New System.Drawing.Point(399, 209)
+        Me.lbKdGuthaben.Name = "lbKdGuthaben"
+        Me.lbKdGuthaben.Size = New System.Drawing.Size(57, 20)
+        Me.lbKdGuthaben.TabIndex = 3
+        Me.lbKdGuthaben.Text = "Label2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lbKdGuthaben)
+        Me.Controls.Add(Me.lbKdDaten)
+        Me.Controls.Add(Me.cmdAddGuthaben)
+        Me.Controls.Add(Me.cmdDaten)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents cmdDaten As Button
+    Friend WithEvents cmdAddGuthaben As Button
+    Friend WithEvents lbKdDaten As Label
+    Friend WithEvents lbKdGuthaben As Label
 End Class
--- 2019-05-24 Kunden/Form1.vb
+++ 2019-05-24 Kunden/Form1.vb
@@ -1,3 +1,97 @@
 ﻿Public Class Form1
+    Inherits System.Windows.Forms.Form
+    Private oKunde1 As CKunde
+    Private Sub cmdDaten_Click(sender As Object, e As EventArgs) Handles cmdDaten.Click
+
+        Dim oKunde1 As New CKunde
+        oKunde1.pAnrede = "Herr"
+        oKunde1.pName = "Müller"
+        oKunde1.pPLZ = "12345"
+        oKunde1.pOrt = "Berlin"
+        oKunde1.pStammkunde = True
+        oKunde1.pGuthaben = 10
+
+        lbKdDaten.Text = oKunde1.mthAdresse()
+        oKunde1.mthGuthabenMan(50)
+        lbKdGuthaben.Text = "Guthaben beträgt:" & oKunde1.pGuthaben & "€."
+
+
+    End Sub
 
 End Class
+
+Public Class CKunde
+    Dim oKunde As New CKunde
+    Private mAnrede As String
+    Private mName As String
+    Private mPLZ As String
+    Private mOrt As String
+    Private mStammkunde As Boolean
+    Private mGuthaben As Integer
+    Sub New()
+
+    End Sub
+    Public Property pAnrede As String
+        Get
+            Return mAnrede
+        End Get
+        Set(ByVal prmAnrede As String)
+            mAnrede = prmAnrede
+        End Set
+    End Property
+    Public Property pName As String
+        Get
+            Return mName
+        End Get
+        Set(ByVal prmName As String)
+            mName = prmName
+        End Set
+    End Property
+    Public Property pPLZ As String
+        Get
+            Return mPLZ
+        End Get
+        Set(ByVal prmPLZ As String)
+            mPLZ = prmPLZ
+        End Set
+    End Property
+    Public Property pOrt As String
+        Get
+            Return mOrt
+        End Get
+        Set(ByVal prmOrt As String)
+            mOrt = prmOrt
+        End Set
+    End Property
+    Public Property pStammkunde As Boolean
+        Get
+            Return mStammkunde
+        End Get
+        Set(ByVal prmStammkunde As Boolean)
+            mStammkunde = prmStammkunde
+        End Set
+    End Property
+    Public Property pGuthaben As Integer
+        Get
+            Return mGuthaben
+        End Get
+        Set(ByVal prmGuthaben As Integer)
+            mGuthaben = prmGuthaben
+        End Set
+    End Property
+    Public Sub mthGuthabenMan(ByVal prmBetrag As Integer)
+        If mStammkunde = True Then
+            mGuthaben = mGuthaben + prmBetrag
+        End If
+    End Sub
+    Public Function mthAdresse()
+        Return mAnrede & "" & mName & vbCrLf & mPLZ & "" & mOrt
+    End Function
+End Class
+
+
+
+
+
+
+
